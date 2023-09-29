4f16e3433de3ef2c95f3bc5aca6f869b367db6bc
Saurabh Kumar Verma <saurabh.verma@pw.live>
2023-09-30 00:37:00 +0530

    having conficting lines

--- a.cpp
+++ a.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main() {
-    cout<<"Hello World \n";
-    cout<<"Hi, How are your \n";
-    cout<<"This can cause conflict";
+    cout<<"Hello  World \n";
+    cout<<"Hi, How are  your \n";
+    cout<<"This can cause conflict\n";
+    int x = 5, z = 10;
+    cout<<x+z<<endl;
+    /*
+    */
 }
